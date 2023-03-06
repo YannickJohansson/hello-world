bf688b7509f19431bf4299169f09bfd813448692
LucasArvidsson <03lucarv@skola.boras.se>
2023-03-06 15:14:38 +0100

    Added Apollo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <ApolloExample msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import ApolloExample from './components/ApolloExample.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    ApolloExample,
   },
 })
 export default class App extends Vue {}
